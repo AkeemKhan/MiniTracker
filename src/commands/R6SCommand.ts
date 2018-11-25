6155d3deb870632d73bb2b63d0e0184a2d05ac77
AkeemKhan <akeemkhan@outlook.com>
2018-11-25 18:45:50 +0000

    Working basic Siege tracker

--- src/commands/R6SCommand.ts
+++ src/commands/R6SCommand.ts
@@ -5,25 +5,44 @@ const config = require("../config.json");
 
 export class R6SCommand implements ICommand {   
     public handleCommand(message: discord.Message){
-        let args = message.content.slice(config.prefix.length).trim().split(" ");
-        console.log("Args - " + args + " " + args.length + " " + config.prefix);
 
+        let args = message.content.slice(config.prefix.length).trim().split(" ");
         const Scout = require("@scoutsdk/server-sdk");
-        let ninja;
     
-        Scout.configure({
-            clientId: config.clientId,
-            clientSecret: config.clientSecret,
-            scope: "public.read"
-        }).then(async () => {
-            let titles = await Scout.titles.list();
-            let siege = titles.find(t => t.slug === "r6siege");
-            let search = await Scout.players.search("MasterZet_", "uplay", siege.id, true, true);
-            console.log(search);
-            // search.results[0].persona.id
-            let char = await Scout.players.get("r6siege", search.results[0].persona.id);
-            console.log(char);
-            message.channel.send("Not implemented")
-        });
+        if (args.length > 1) {
+            
+            let name = args[1];
+
+            Scout.configure({
+                clientId: config.clientId,
+                clientSecret: config.clientSecret,
+                scope: "public.read"
+            }).then(async () => {
+                let titles = await Scout.titles.list();
+                let siege = titles.find(t => t.slug === "r6siege");
+                let search = await Scout.players.search(name, "uplay", siege.id, true, true);
+
+                if (search == null){
+                    message.channel.send("Search criteria failed");
+                    console.log("Search criteria failed");
+                }
+                else {
+                    message.channel.send("Searching...");
+                    console.log("Searching...");
+                    let char = await Scout.players.get("r6siege", search.results[0].persona.id).then((result) => {                        
+                        let playerStatsOutput = "```\n";  
+                            
+                        for (let entry of result.stats) {
+                            playerStatsOutput += "\n" + entry.metadata["name"] + ": " + entry.value;
+                        }
+                            
+                        message.channel.send(playerStatsOutput + "\n```");
+                    }).catch((err) => {
+                        message.channel.send("Search Player failed");
+                        console.log("Search Player failed");
+                    });;
+                }
+            });            
+        }
     }
 }
